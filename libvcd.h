720dade971cff9fc2230e8e63339dc47e1ce4a52
Soroush Sherafat <sorousherafat@gmail.com>
2023-05-02 01:38:39 +0330

    added `typedef` and `struct` definitions

--- libvcd.h
+++ libvcd.h
@@ -5,4 +5,20 @@
 
 typedef uint64_t timestamp_t;
 
+typedef struct {
+    timestamp_t timestamp;
+    char *value;
+} value_change_t;
+
+typedef struct {
+    char *name;
+    value_change_t *value_changes;
+} signal_vcd_t;
+
+typedef struct {
+    signal_vcd_t *signal_dumps;
+    char *date;
+    char *version;
+} vcd_t;
+
 #endif //LIBVCD_LIBVCD_H
