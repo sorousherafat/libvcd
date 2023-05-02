c805bda4da3c47a24e9ed50886a97cd15d9ff74d
Soroush Sherafat <sorousherafat@gmail.com>
2023-05-02 15:15:04 +0330

    made `file` and `vcd` local

--- libvcd.c
+++ libvcd.c
@@ -7,36 +7,34 @@
 
 #define isexpression(c) (strchr("-0123456789zZxXbU", (c)))
 
-static FILE *file;
-static vcd_t *vcd;
-
-static void init_vcd(void);
-static void parse_var(void);
-static void parse_timestamp(void);
-static void parse_assignment(void);
+static void init_vcd(vcd_t **vcd);
+static void parse_var(FILE *file, vcd_t *vcd);
+static void parse_timestamp(FILE *file, vcd_t *vcd);
+static void parse_assignment(FILE *file, vcd_t *vcd);
 
 vcd_t *open_vcd(char *path) {
-    file = fopen(path, "r");
+    FILE *file = fopen(path, "r");
     if (file == NULL)
         return NULL;
 
-    init_vcd();
+    vcd_t *vcd;
+    init_vcd(&vcd);
 
     int c;
     while ((c = fgetc(file)) != EOF) {
         if (c == '$') {
-            parse_var();
+            parse_var(file, vcd);
             continue;
         }
 
         if (c == '#') {
-            parse_timestamp();
+            parse_timestamp(file, vcd);
             continue;
         }
 
         if (isexpression(c)) {
             ungetc(c, file);
-            parse_assignment();
+            parse_assignment(file, vcd);
             continue;
         }
 
@@ -51,22 +49,22 @@ vcd_t *open_vcd(char *path) {
     }
 }
 
-void init_vcd() {
-    vcd = (vcd_t *) malloc(sizeof(vcd_t));
-    vcd->version = (char *) malloc(LIBVCD_VERSION_SIZE);
-    vcd->date = (char *) malloc(LIBVCD_DATE_SIZE);
-    vcd->timescale.unit = (char *) malloc(LIBVCD_TIMESCALE_UNIT_SIZE);
-    vcd->signal_dumps = (signal_t *) malloc(LIBVCD_MAX_SIGNAL_COUNT * sizeof(signal_t));
+void init_vcd(vcd_t **vcd) {
+    *vcd = (vcd_t *) malloc(sizeof(vcd_t));
+    (*vcd)->version = (char *) malloc(LIBVCD_VERSION_SIZE);
+    (*vcd)->date = (char *) malloc(LIBVCD_DATE_SIZE);
+    (*vcd)->timescale.unit = (char *) malloc(LIBVCD_TIMESCALE_UNIT_SIZE);
+    (*vcd)->signal_dumps = (signal_t *) malloc(LIBVCD_MAX_SIGNAL_COUNT * sizeof(signal_t));
 }
 
-void parse_var(void) {
-
-}
-
-void parse_timestamp(void) {
+void parse_var(FILE *file, vcd_t *vcd) {
 
 }
 
-void parse_assignment(void) {
+void parse_timestamp(FILE *file, vcd_t *vcd) {
+
+}
+
+void parse_assignment(FILE *file, vcd_t *vcd) {
 
 }
