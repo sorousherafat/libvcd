6a8a39102d06a0356a2d1bb17887be8d85504085
Soroush Sherafat <sorousherafat@gmail.com>
2023-05-16 00:51:16 +0330

    made all dynamic arrays static

--- libvcd.h
+++ libvcd.h
@@ -4,33 +4,39 @@
 #include <stdint.h>
 #include <stdio.h>
 
-#define LIBVCD_MAX_SIGNAL_COUNT 64
-#define LIBVCD_MAX_VALUE_CHANGE_COUNT 2048
+#define LIBVCD_SCOPE_COUNT 8
+#define LIBVCD_SCOPE_SIGNAL_COUNT 32
+#define LIBVCD_VALUE_CHANGE_COUNT 512
+#define LIBVCD_SIGNAL_SIZE 128
+#define LIBVCD_NAME_SIZE 32
+#define LIBVCD_TIME_UNIT_SIZE 8
+#define LIBVCD_VERSION_SIZE 64
+#define LIBVCD_DATE_SIZE 64
 
 typedef uint32_t timestamp_t;
 
 typedef struct {
     timestamp_t timestamp;
-    char *value;
+    char value[LIBVCD_SIGNAL_SIZE];
 } value_change_t;
 
 typedef struct {
-    char *name;
+    char name[LIBVCD_NAME_SIZE];
     size_t size;
-    value_change_t *value_changes;
+    value_change_t value_changes[LIBVCD_VALUE_CHANGE_COUNT];
     size_t changes_count;
 } signal_t;
 
 typedef struct {
-    char *unit;
+    char unit[LIBVCD_TIME_UNIT_SIZE];
     size_t scale;
 } timescale_t;
 
 typedef struct {
-    signal_t *signal_dumps;
     size_t signals_count;
-    char *date;
-    char *version;
+    signal_t signals[LIBVCD_SCOPE_SIGNAL_COUNT];
+    char date[LIBVCD_DATE_SIZE];
+    char version[LIBVCD_VERSION_SIZE];
     timescale_t timescale;
 } vcd_t;
 
